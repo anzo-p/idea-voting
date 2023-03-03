bcac2fb1dc4633b8c5c837089abe38e0b6c1bec6
anzo-p <anssi.pitkan@gmail.com>
2023-03-03 22:34:37 +0200

    complete createBoard tests

--- src/functions/createBoard/index.unit.test.ts
+++ src/functions/createBoard/index.unit.test.ts
@@ -63,4 +63,51 @@ describe("createBoard", () => {
     expect(puCommand.Item.ownerId).toEqual("123");
     expect(puCommand.Item.pk).toEqual("board");
   });
+
+  test("201 success - anonymous", async () => {
+    // @ts-ignore
+    const event: APIGatewayProxyEvent = {
+      body: JSON.stringify({
+        name: "boardName",
+        description: "boardDescription",
+        isPublic: false,
+      }),
+    };
+
+    const response = await handler(event);
+    const responseBody = JSON.parse(response.body);
+    const puCommand = mockSend.send.mock.calls[0][0].input;
+
+    expect(response.statusCode).toBe(201);
+    expect(responseBody.message).toBe("board created");
+    expect(mockSend.send).toBeCalledTimes(1);
+    expect(puCommand.Item.ownerId).toEqual("anonymous");
+  });
+
+  test("400 bad request - on invalid payload will not store into db", async () => {
+    // @ts-ignore
+    const event: APIGatewayProxyEvent = {
+      body: JSON.stringify({
+        description: "boardDescription",
+        isPublic: false,
+      }),
+    };
+
+    const response = await handler(event);
+    const responseBody = JSON.parse(response.body);
+
+    expect(response.statusCode).toBe(400);
+    expect(responseBody.message).toBe("'name' is required'");
+    expect(mockSend.send).toBeCalledTimes(0);
+  });
+
+  test("500 bad request - on general error will not store into db", async () => {
+    // @ts-ignore
+    const event: APIGatewayProxyEvent = {};
+
+    const response = await handler(event);
+
+    expect(response.statusCode).toBe(500);
+    expect(mockSend.send).toBeCalledTimes(0);
+  });
 });
